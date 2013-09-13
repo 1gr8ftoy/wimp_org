aa0eba0c2c1eb45eebfb5cb9dbc0b83f0a0b0a2d
Brandon Conway <brandoncc@gmail.com>
2013-09-12 23:22:11 -0700

    Configured LiipImagineBundle

--- Symfony/app/AppKernel.php
+++ Symfony/app/AppKernel.php
@@ -23,6 +23,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new BConway\WebsiteBundle\BConwayWebsiteBundle(),
+            new Liip\ImagineBundle\LiipImagineBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
