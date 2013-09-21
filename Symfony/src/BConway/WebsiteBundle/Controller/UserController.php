129288b7610ab8f910d9b415a308d70d66b377f7
Brandon Conway <brandoncc@gmail.com>
2013-09-21 14:26:49 -0700

    Wrote User actions

--- Symfony/src/BConway/WebsiteBundle/Controller/UserController.php
+++ Symfony/src/BConway/WebsiteBundle/Controller/UserController.php
@@ -2,8 +2,42 @@
 
 namespace BConway\WebsiteBundle\Controller;
 
+use BConway\WebsiteBundle\Form\Type\DeleteAccountType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class UserController extends Controller
 {
+    public function deleteAction(Request $request)
+    {
+        $form = $this->createForm(new DeleteAccountType());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $this->getUser();
+
+            $this->get('fos_user.user_manager')->deleteUser($user);
+
+            $request->getSession()->invalidate();
+
+            return $this->redirect($this->generateUrl('b_conway_website_homepage'));
+        }
+
+        return $this->render('BConwayWebsiteBundle:User:delete_account.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    public function postsAction()
+    {
+        $user = $this->getUser();
+        $em = $this->getDoctrine()->getManager();
+
+        $lostPets = $em->getRepository('BConwayWebsiteBundle:LostPet')->findByUser($user);
+        $foundPets = $em->getRepository('BConwayWebsiteBundle:FoundPet')->findByUser($user);
+        return $this->render('BConwayWebsiteBundle:User:edit_posts.html.twig', array(
+            'lostPets' => $lostPets,
+            'foundPets' => $foundPets,
+        ));
+    }
 }
